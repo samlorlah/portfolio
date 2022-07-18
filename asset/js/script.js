1b4c6251025ce1ce4b424c7f4ff0f3962338339b
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-07-18 16:08:14 +0100

    linters error corrected

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,16 +1,16 @@
-let hamburger = document.getElementById("hamburger");
-let closeMenu = document.getElementById("close-menu-icon");
-let mobileMenu = document.getElementById("mobile-menu");
-let menuList = document.querySelector(".menu-list");
+const hamburger = document.getElementById('hamburger');
+const closeMenu = document.getElementById('close-menu-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuList = document.querySelector('.menu-list');
 
-hamburger.addEventListener("click", function () {
-  mobileMenu.style.display = "block";
+hamburger.addEventListener('click', function () {
+  mobileMenu.style.display = 'block';
 });
 
-closeMenu.addEventListener("click", function () {
-  mobileMenu.style.display = "none";
+closeMenu.addEventListener('click', function () {
+  mobileMenu.style.display = 'none';
 });
 
-menuList.addEventListener("click", function () {
-  mobileMenu.style.display = "none";
+menuList.addEventListener('click', function () {
+  mobileMenu.style.display = 'none';
 });
