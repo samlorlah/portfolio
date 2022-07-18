7f2327165d5e5a59f3512c5243c39b3d202d9740
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-07-18 15:58:00 +0100

    add Event Listener to hamburger to open menu
    add event listener to close button to close menu
    
    add even listen to close menu n menu list click
    
    add section links to menu href

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,16 @@
 let hamburger = document.getElementById("hamburger");
+let closeMenu = document.getElementById("close-menu-icon");
+let mobileMenu = document.getElementById("mobile-menu");
+let menuList = document.querySelector(".menu-list");
 
 hamburger.addEventListener("click", function () {
-  console.log("Clicked");
+  mobileMenu.style.display = "block";
+});
+
+closeMenu.addEventListener("click", function () {
+  mobileMenu.style.display = "none";
+});
+
+menuList.addEventListener("click", function () {
+  mobileMenu.style.display = "none";
 });
