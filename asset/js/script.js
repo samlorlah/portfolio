fb2c28a7c931a765964b956d7587912aa5cad717
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-07-19 17:49:58 +0100

    Javascript Linters error corrected

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,36 +1,36 @@
-const hamburger = document.getElementById("hamburger");
-const closeMenu = document.getElementById("close-menu-icon");
-const mobileMenu = document.getElementById("mobile-menu");
-const menuList = document.querySelector(".menu-list");
-const modalClose = document.querySelectorAll(".modal-close");
-const modalDialog = document.querySelectorAll(".popup");
-const modals = document.querySelectorAll(".action-btn");
+const hamburger = document.getElementById('hamburger');
+const closeMenu = document.getElementById('close-menu-icon');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuList = document.querySelector('.menu-list');
+const modalClose = document.querySelectorAll('.modal-close');
+const modalDialog = document.querySelectorAll('.popup');
+const modals = document.querySelectorAll('.action-btn');
 
-hamburger.addEventListener("click", () => {
-  mobileMenu.style.display = "block";
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = 'block';
 });
 
-closeMenu.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
+closeMenu.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
 });
 
-menuList.addEventListener("click", () => {
-  mobileMenu.style.display = "none";
+menuList.addEventListener('click', () => {
+  mobileMenu.style.display = 'none';
 });
 
-for (let i = 0; i < modalClose.length; i++) {
-  modalClose[i].addEventListener("click", () => {
-    for (let j = 0; j < modalDialog.length; j++) {
-      if (modalDialog[j].style.display == "block") {
-        modalDialog[j].style.display = "none";
+for (let i = 0; i < modalClose.length; i += 1) {
+  modalClose[i].addEventListener('click', () => {
+    for (let j = 0; j < modalDialog.length; j += 1) {
+      if (modalDialog[j].style.display == 'block') {
+        modalDialog[j].style.display = 'none';
       }
     }
   });
 }
 
-for (let i = 0; i < modals.length; i++) {
-  modals[i].addEventListener("click", () => {
-    const modal = document.querySelector(modals[i].getAttribute("data-target"));
-    modal.style.display = "block";
+for (let i = 0; i < modals.length; i += 1) {
+  modals[i].addEventListener('click', () => {
+    const modal = document.querySelector(modals[i].getAttribute('data-target'));
+    modal.style.display = 'block';
   });
 }
