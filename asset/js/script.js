6dc18aa9f29a0e1551b142e47a98c9938cc401cb
Kehinde Onifade <onifkay@gmail.com>
2022-07-19 17:31:22 +0100

    Add event listener to display popup button

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,20 +1,21 @@
-const hamburger = document.getElementById('hamburger');
-const closeMenu = document.getElementById('close-menu-icon');
-const mobileMenu = document.getElementById('mobile-menu');
-const menuList = document.querySelector('.menu-list');
+const hamburger = document.getElementById("hamburger");
+const closeMenu = document.getElementById("close-menu-icon");
+const mobileMenu = document.getElementById("mobile-menu");
+const menuList = document.querySelector(".menu-list");
 const modalClose = document.querySelectorAll(".modal-close");
 const modalDialog = document.querySelectorAll(".popup");
+const modals = document.querySelectorAll(".action-btn");
 
-hamburger.addEventListener('click', () => {
-  mobileMenu.style.display = 'block';
+hamburger.addEventListener("click", () => {
+  mobileMenu.style.display = "block";
 });
 
-closeMenu.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
+closeMenu.addEventListener("click", () => {
+  mobileMenu.style.display = "none";
 });
 
-menuList.addEventListener('click', () => {
-  mobileMenu.style.display = 'none';
+menuList.addEventListener("click", () => {
+  mobileMenu.style.display = "none";
 });
 
 for (let i = 0; i < modalClose.length; i++) {
@@ -24,5 +25,12 @@ for (let i = 0; i < modalClose.length; i++) {
         modalDialog[j].style.display = "none";
       }
     }
+  });
+}
+
+for (let i = 0; i < modals.length; i++) {
+  modals[i].addEventListener("click", () => {
+    const modal = document.querySelector(modals[i].getAttribute("data-target"));
+    modal.style.display = "block";
   });
 }
