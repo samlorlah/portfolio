6f2f760b679f5336e0feaa0cbc6e47bbb2e15af2
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-07-19 17:28:05 +0100

    add event listener to modal close button

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,6 +2,8 @@ const hamburger = document.getElementById('hamburger');
 const closeMenu = document.getElementById('close-menu-icon');
 const mobileMenu = document.getElementById('mobile-menu');
 const menuList = document.querySelector('.menu-list');
+const modalClose = document.querySelectorAll(".modal-close");
+const modalDialog = document.querySelectorAll(".popup");
 
 hamburger.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
@@ -14,3 +16,13 @@ closeMenu.addEventListener('click', () => {
 menuList.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
 });
+
+for (let i = 0; i < modalClose.length; i++) {
+  modalClose[i].addEventListener("click", () => {
+    for (let j = 0; j < modalDialog.length; j++) {
+      if (modalDialog[j].style.display == "block") {
+        modalDialog[j].style.display = "none";
+      }
+    }
+  });
+}
