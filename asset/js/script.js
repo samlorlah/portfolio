cc94074d98a7b3205d4ac249aeed508d4a06695e
Muyiwa Fawole <fawolemuyiwa@rocketmail.com>
2022-07-20 22:09:39 +0100

    correct Linters errors

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,7 +6,6 @@ const modalClose = document.querySelectorAll('.modal-close');
 const modalDialog = document.querySelectorAll('.popup');
 const modals = document.querySelectorAll('.action-btn');
 const contactForm = document.getElementById('contact-me-form');
-const contactFormSubmitBtn = document.getElementById('contact-form-submit-btn');
 
 hamburger.addEventListener('click', () => {
   mobileMenu.style.display = 'block';
@@ -38,17 +37,22 @@ for (let i = 0; i < modals.length; i += 1) {
 }
 
 const validateEmail = (email) => {
-  return email !== email.toLowerCase() ? false : true;
+  if (email !== email.toLowerCase())
+  {
+    return false
+  } else {
+    return true;
+  }
 };
 
 contactForm.addEventListener('submit', (e) => {
   const EMAIL_INVALID = 'Your Email should be in lower case only. Please try again!';
   const displayMsg = document.getElementById('display-msg');
   e.preventDefault();
-  if(! validateEmail(contactForm.elements.email.value.trim())){
+  if (!validateEmail(contactForm.elements.email.value.trim())) {
     displayMsg.innerText = EMAIL_INVALID;
     displayMsg.style.color = '#ff0000';
-  }else {
+  } else {
     displayMsg.innerText = '';
     contactForm.submit();
   }
